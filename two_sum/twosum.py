559f4978e511fca65ef1f63453b9ecd2a81b6f09
qingyelvluo <2256280155@qq.com>
2021-09-17 13:01:05 +0800

    两数之和v1版本(已通过)

--- two_sum/twosum.py
+++ two_sum/twosum.py
@@ -1,37 +1,35 @@
-'''
+#coding: utf8
+
+# 解题思路：
+# 1、先通过数组下标 元素构成二维数组，存入列表
+# 2、对二维数组排序（以元素排序，对应下标自动保留记录）
+# 3、使用双指针对有序数组分别遍历
+# 优点：
+#   思路清晰
+# 缺点：
+#   空间复杂度很高（多高？）
+
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        # sort array first
-        #nums = sorted(nums)
-        start, end = 0, len(nums)-1
+        tmp_list = []
+        # 以数组下标 元素构成二维数组，放入列表
+        for i, j in enumerate(nums):
+            tmp_list.append([i, j])
+        # 先对二维数组排序（以元素排序，记录下来下标）
+        tmp_list = sorted(tmp_list, key=lambda x: x[1])
+        start, end = 0, len(tmp_list)-1
+        # 双指针前后分别遍历
         while start < end:
-            if nums[start] + nums[end] == target:
-                return [start, end]
-            elif nums[start] + nums[end] > target:
+            if tmp_list[start][1] + tmp_list[end][1] == target:
+                return [tmp_list[start][0], tmp_list[end][0]]
+            elif tmp_list[start][1] + tmp_list[end][1] > target:
                 end -= 1
-            elif nums[start] + nums[end] < target:
+            elif tmp_list[start][1] + tmp_list[end][1] < target:
                 start += 1
         return None
-if __name__ == "__main__":
-    nums = [3,2,4]
-    target = 6
-    obj = Solution()
-    print(obj.twoSum(nums, target))
-'''
-class Solution(object):
-    def twoSum(self, nums, target):
-        tmp_dict = {}
-        for i, j in enumerate(nums):
-            tmp_dict[i] = j
-        return tmp_dict
 
 if __name__ == "__main__":
     nums = [3,2,4]
-    target = 6
+    target = 5
     obj = Solution()
     print(obj.twoSum(nums, target))
